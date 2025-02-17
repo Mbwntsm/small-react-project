ba5662d827904e7e1137d99492970cfabdfe47f9
metroooO00! <143833978+Mbwntsm@users.noreply.github.com>
2025-02-18 01:10:54 +0530

    Meal menu project

---
+++ Meals/Meals.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import axios from "axios";
+import { useState,useEffect } from 'react';
+import './style.css'
+
+const Meals = () => {
+  const [meals,setMeals] = useState([]);
+  
+
+  useEffect(()=>{
+    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then((res)=>{
+      //console.log(res.data.meals);
+      setMeals(res.data.meals);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+
+  },[])
+
+  const itemslist = meals.map(({strMeal,strMealThumb,idMeal})=>{
+    return (
+      <section className='card'>
+        <img src={strMealThumb} alt={strMeal}/>
+        <section className='content'>
+          <p>{strMeal}</p>
+          <p>{idMeal}</p>
+        </section>
+      </section>
+    )
+  })
+
+  return <div className='items-container'>{itemslist}</div>
+}
+
+export default Meals
